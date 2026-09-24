fa29f983d288f23cb89261b0dfdf2bd3d808aa79
SONZ-INA <aleksakostic034@gmail.com>
2026-09-24 20:12:20 +0200

    Initial push of icon framer mini-program

---
+++ frame_icons.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+title Icon Framer
+
+echo ========================================
+echo           ICON FRAMER
+echo ========================================
+echo.
+
+python "%~dp0frame_icons.py"
